3e07111e3122caeab2a10f77d6b5857433be2206
Dannnex <dannycapps0419@gmail.com>
2024-09-16 11:57:10 -0700

    Prep 3 Assignment completed.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string playAgain = "Y";
+        do{
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 100);
+            int userGuess;
+            int numOfGuesses = 0;
+            do
+            {
+                Console.Write("Guess a number! ");
+                string userInputGuess = Console.ReadLine();
+                userGuess = int.Parse(userInputGuess);
+                numOfGuesses++;
+                if (userGuess > magicNumber)
+                {
+                    Console.WriteLine("Lower!");
+                }
+                else if (userGuess < magicNumber)
+                {
+                    Console.WriteLine("Higher!");
+                }
+            } while (userGuess != magicNumber);
+            Console.WriteLine($"You guessed it! It took you {numOfGuesses} guesses.");
+            Console.WriteLine("Play again? ");
+            playAgain = Console.ReadLine();
+        } while (playAgain == "Y");
     }
 }
