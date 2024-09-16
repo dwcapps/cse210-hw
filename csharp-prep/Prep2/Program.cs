29f0e92d768320c274084e32f464d1b7fe4f7180
Dannnex <dannycapps0419@gmail.com>
2024-09-16 03:31:50 -0700

    Prep 2 assignment completed.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,63 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade? ");
+        string percentInText = Console.ReadLine();
+        int percent = int.Parse(percentInText);
+        string letter = "Error";
+        string sign = "";
+
+        if (percent > 90)
+        {
+            letter = "A";
+        }
+        else if (percent > 80)
+        {
+            letter = "B";
+        }
+        else if (percent > 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else if (percent < 60)
+        {
+            letter = "F";
+        }
+        
+        int signNum = percent % 10;
+
+        if (signNum >= 7)
+        {
+            sign = "+";
+        }
+        else if (signNum < 3)
+        {
+            sign = "-";
+        }
+
+        if (percent > 93)
+        {
+            sign = "";
+        }
+        else if (percent < 60)
+        {
+            sign = "";
+        }
+
+        Console.Write(letter);
+        Console.WriteLine(sign);
+
+        if (percent > 70)
+        {
+            Console.WriteLine("Congrats, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you did not pass. You'll get it next time!");
+        }
     }
 }
